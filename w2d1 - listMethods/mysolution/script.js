417b1da4b26e3b421de76bf9e03acc34e2b24b31
bertea74@gmail.com <bertea74@gmail.com>
2018-09-04 11:11:30 +0200

    no message

---
+++ w2d1 - listMethods/mysolution/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+function listMethods(id) {
+  let lis = document.querySelectorAll("#" + id + " li");
+
+  lis.forEach(li => {
+    displayElement(li);
+  });
+}
+
+function listMethodsFor(id) {
+  const lis = document.querySelectorAll("#" + id + " li");
+  for (let i = 0; i < lis.length; i++) {
+    displayElement(lis[i]);
+  }
+}
+
+function displayElement(element) {
+  console.log(element.textContent);
+}
+listMethods("info");
+console.log("+++");
+listMethods("modify");
+console.log("+++");
+listMethods("newarray");
+
+console.log("======================");
+listMethodsFor("info");
+console.log("+++");
+listMethodsFor("modify");
+console.log("+++");
+listMethodsFor("newarray");
